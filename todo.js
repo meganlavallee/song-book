ec77a2c6825cd83b73ee0da21b07207eb5d27d11
Megan Lavallee <megan.lavallee@yahoo.com>
2020-12-17 11:57:19 -0700

    requires, schema

--- todo.js
+++ todo.js
@@ -1,4 +1,21 @@
-// Table: Song, Score
-// ROUTES: Add, Read, Update, Delete
-// Setting up Connection
-// Model of a playlist/post
+// TODO: Table: Song, Score
+// TODO: ROUTES: Add, Read, Update, Delete
+// TODO: Setting up Connection
+// TODO: Model of a playlist/post
+// TODO: Config.json file
+
+var Sequelize = require('sequelize');
+const sequelize = require("sequelize");
+const sequelizecli = require("sequelize-cli")
+const express = require("express");
+const expresshandlebars = require("express-handlebars");
+const mysql = require("mysql");
+
+
+var connection = new Sequelize
+('sequelize_songbook', 'root', '', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'sequelize',
+  });
+
