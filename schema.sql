0d899f67da99886e8df0344333e15da7b88e8928
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-22 11:17:49 -0700

    fix schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,1 @@
 CREATE DATABASE songbook;
-CREATE TABLE'playlists';(
-    id int NOT NULL primary key AUTO_INCREMENT
-    name VARCHAR(100)
-    score INT
-    url varchar (100) 
-) default charset utf8 
